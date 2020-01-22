c05afa4c347b8fa3799051b88cb018f654356b8c
Tsubasa Imamura <imamura@mk.ces.kyutech.ac.jp>
2020-01-22 22:44:29 +0900

    add

--- add/js/main.js
+++ add/js/main.js
@@ -8,3 +8,22 @@ $(function(){
 		return false;
 	});	
 });
+(function($) {
+ 
+$(function () {
+    var display = function () {
+    if ($(this).scrollTop() > 150) { //scroll量
+            $(".Bnr").fadeIn();
+        } else {
+            $(".Bnr").fadeOut();
+        }
+    };
+    $(window).on("scroll", display);
+    //click
+    $(".Bnr p.close a").click(function(){
+    $(".Bnr").fadeOut();
+    $(window).off("scroll", display);
+    });
+});
+ 
+})(jQuery);
