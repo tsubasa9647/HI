bfb4db4d86a95ec9daac47c7ed34ade9b9c20971
Tsubasa Imamura <imamura@mk.ces.kyutech.ac.jp>
2020-02-04 18:08:24 +0900

    add

---
+++ add/js/sidebar.js
@@ -0,0 +1,41 @@
+﻿$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+    
+    var dropdownlink = this.el.find('.dropdownlink');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+  };
+  
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the ul.submenuItems
+        $next = $this.next();
+    
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+    
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+  
+  var accordion = new Accordion($('.accordion-menu'), false);
+})
+
+$(function(){
+  //.accordion_oneの中の.accordion_headerがクリックされたら
+  $('.s_02 .accordion_one .accordion_header').click(function(){
+    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+    $(this).next('.accordion_inner').slideToggle();
+    $(this).toggleClass("open");
+    //クリックされた.accordion_oneの中の.accordion_header以外の.accordion_oneの中の.accordion_headerに隣接する.accordion_oneの中の.accordion_innerを閉じる
+    $('.s_02 .accordion_one .accordion_header').not($(this)).next('.accordion_one .accordion_inner').slideUp();
+    $('.s_02 .accordion_one .accordion_header').not($(this)).removeClass("open");
+  });
+});
